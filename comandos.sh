b9d4d014ee4ab8761b46a49a6d8526e335f0ef05
milliyx <143279616+milliyx@users.noreply.github.com>
2023-09-21 23:46:00 -0600

    [MOD]21092023
    se añade script con comandos personalizados para la terminal

---
+++ comandos.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Función para manejar la señal SIGINT (Ctrl+C)
+interrumpir() {
+    echo "Presione 'salir' para salir del programa."
+}
+
+# Función para manejar la señal SIGTSTP (Ctrl+Z)
+suspender() {
+    echo "El programa no puede ser suspendido. Presione 'salir' para salir del programa."
+}
+
+# Configurar manejo de señales
+trap 'interrumpir' SIGINT
+trap 'suspender' SIGTSTP
+
+# Bucle do-while
+while true; do
+    usuario=$(whoami)
+    ruta_actual=$(pwd)
+
+    # Definir colores ANSI
+    color_usuario="\e[1;33m"  # Amarillo brillante
+    color_ruta="\e[1;34m"      # Azul brillante
+    color_reset="\e[0m"        # Restablecer el color
+    flechita='\e[1;31m'
+
+    # Mostrar el prompt simulado con colores y flecha
+    echo -ne "${color_usuario}$usuario@$(hostname):${color_ruta}$ruta_actual${flechita} > ${color_reset}"
+
+    read -p "" opcion
+
+    # Utilizar un caso (case) para manejar las opciones
+    case "$opcion" in
+        "art")
+            # Ejecutar art.sh
+            ./art.sh
+            ;;
+        "infosis")
+            # Ejecutar infosis.sh
+            ./infosis.sh
+            ;;
+        "who")
+            # Ejecutar who.sh
+            ./who.sh
+            ;;
+        "juego")
+            # Ejecutar juego.sh
+            ./juego.sh
+            ;;
+        "music")
+            # Ejecutar music.sh
+            ./music.sh
+            ;;
+        "time")
+            # Ejecutar time.sh
+            ./time.sh
+            ;;
+        "ayuda")
+            # Ejecutar ayuda.txt
+            cat ayuda.txt
+            ;;
+        "find")
+            # Ejecutar find.sh
+            ./find.sh
+            ;;
+        "salir")
+            # Salir del bucle si el usuario ingresa "salir"
+            break
+            ;;
+        *)
+            # Mostrar un mensaje de error para opciones no válidas
+            echo "Opción no válida. Por favor, ingrese una opción válida."
+            ;;
+    esac
+done
+
+# Mensaje de salida al final del script
+echo "El programa ha finalizado."
